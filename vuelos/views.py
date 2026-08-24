9547235009f90e9ed3c0f6a232816feacee14eef
Pablo Pissi <pablopissi@gmail.com>
2026-08-23 22:42:39 -0300

    Agrego endpoint de venta de vuelos

--- vuelos/views.py
+++ vuelos/views.py
@@ -1,8 +1,11 @@
-from rest_framework.viewsets import ReadOnlyModelViewSet
+from django.db import transaction
+from django.db.models import F
+from rest_framework.mixins import CreateModelMixin
+from rest_framework.viewsets import ReadOnlyModelViewSet, GenericViewSet
 
 from vuelos.filters import VueloFilterSet
-from vuelos.models import Vuelo
-from vuelos.serializers import VueloSerializer
+from vuelos.models import Vuelo, VentaVuelo
+from vuelos.serializers import VueloSerializer, VentaVueloSerializer
 
 
 class VueloViewSet(ReadOnlyModelViewSet):
@@ -12,3 +15,15 @@ class VueloViewSet(ReadOnlyModelViewSet):
 
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
+
+
+class VentaVueloViewSet(CreateModelMixin, GenericViewSet):
+    queryset = VentaVuelo.objects.all()
+    serializer_class = VentaVueloSerializer
+
+    @transaction.atomic()
+    def perform_create(self, serializer):
+        super().perform_create(serializer)
+        vuelo: Vuelo = serializer.validated_data["vuelo"]
+        vuelo.disponibilidad = F("disponibilidad") - 1
+        vuelo.save()
