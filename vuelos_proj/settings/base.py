f625618ea08c11f369b61eaca4541db9e4035f93
Fede <42482452+fedejmartinez@users.noreply.github.com>
2026-09-17 10:56:31 -0300

    Cambia el puerto donde expone la api y genera nuevos destinos (#11)

--- vuelos_proj/settings/base.py
+++ vuelos_proj/settings/base.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0", "host.docker.internal"]
 
 
 # Application definition
--- vuelos_proj/settings/development.py
+++ vuelos_proj/settings/development.py
@@ -9,7 +9,7 @@ SECRET_KEY = "django-insecure-jr3^jk4c*r*goat!a=4nm_1wm5l_xf=6ywsu6)243wwdwsswzc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0", "host.docker.internal"]
 
 
 # Database
